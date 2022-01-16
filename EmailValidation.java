52d860839c2f72a4dd87d04cfeafe31bb1e5feff
Deepikajadhav1114 <96719120+Deepikajadhav1114@users.noreply.github.com>
2022-01-16 22:27:28 +0530

    Add files via upload

---
+++ EmailValidation.java
@@ -0,0 +1,46 @@
+package mainproject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailValidation
+{
+	   public static boolean isValidEmail(String email) {
+		   String regex = "^(.+)@(.+)$";
+		 //initialize the Pattern object
+	       Pattern pattern = Pattern.compile(regex);
+	       Matcher matcher = pattern.matcher(email);
+	       return matcher.matches();
+
+	       
+	   }
+
+	   public static void main(String[] args) {
+	       List<String> emails = new ArrayList<String>();
+	       // valid email addresses
+	       emails.add("deepikajadhav@example.com");
+	       emails.add("deepikaj@example.com");
+	       emails.add("deeps@example.me.org");
+	       //invalid email addresses
+	       emails.add("ruksar.example.com");
+	       emails.add("vishakha..bob@example.com");
+	       emails.add("shubham@.example.com");
+
+	       for (String value : emails) {
+	           System.out.println("The Email address " + value + " is " + (isValidEmail(value) ? "valid" : "invalid"));
+	       }
+	       
+	       System.out.println("Enter any email address to check");
+	       Scanner sc = new Scanner(System.in);
+	       String input = sc.nextLine();
+	       System.out.println("The Email address " + input + " is " + (isValidEmail(input) ? "valid" : "invalid"));
+	       
+	   }
+	   
+
+
+
+}
